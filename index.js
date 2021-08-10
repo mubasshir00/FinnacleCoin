816748edb6d8f9aa626a4afbf03d6fc97603336a
mubasshir00 <marakib178@gmail.com>
2021-08-10 09:59:04 +0600

    progress report 4

--- index.js
+++ index.js
@@ -46,7 +46,11 @@ app.post('/api/transact',(req,res)=>{
                 senderWallet:wallet,recipient,amount
             });
         }else{
-            transaction = wallet.createTransaction({recipient,amount});
+            transaction = wallet.createTransaction({
+                recipient,
+                amount , 
+                chain:blockchain.chain
+            });
         }
        
     }catch(error){
@@ -68,6 +72,17 @@ app.get('/api/mine-transactions',(req,res)=>{
     transactionMiner.mineTransactions();
     res.redirect('/api/blocks');
 });
+
+app.get('/api/wallet-info',(req,res)=>{
+    const address = wallet.publicKey;
+    res.json({
+        address,
+        balance:Wallet.calculateBalance({
+            chain:blockchain.chain,
+            address
+        })
+    })
+})
 
 const syncWithRootState = () => {
     request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error,response,body) => {
