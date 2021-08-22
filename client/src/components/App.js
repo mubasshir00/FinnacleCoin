e2c3839d6c6f3f86532a1e6e7c340fbb9da0163f
mubasshir00 <marakib178@gmail.com>
2021-08-23 00:12:49 +0600

    fetch data from backend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,15 @@
+// import { response } from 'express';
 import React ,{Component} from 'react';
 
 class App extends Component{
 
-    state = {walletInfo : {address: 'fooxv6',balance: 9999} }
+    state = {walletInfo : {} }
+
+    componentDidMount(){
+        fetch('http://localhost:3000/api/wallet-info')
+        .then(response=>response.json())
+        .then(json=>this.setState({walletInfo:json}));
+    }
 
     render(){
 
@@ -10,8 +17,15 @@ class App extends Component{
 
         return (
             <div>
+                <div>
                 <h1>Hello, Welcome to FinnacleCoin</h1>
-                
+                </div>
+                <div>
+                    Address : {address}
+                </div>
+                <div>
+                    Balance : {balance}
+                </div>
             </div>
         );
     }
